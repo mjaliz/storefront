b34d18943862bdb99d3a08b3dbb988cc53f55ed1
mjaliz <mrph14@gmail.com>
2024-10-26 22:13:12 +0330

    serializing objects

---
+++ store/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from store import views
+
+
+urlpatterns = [
+    path("products/", views.product_list),
+    path("products/<int:id>", views.product_details),
+]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
 
-# Create your views here.
+from store.models import Product
+from store.serializers import ProductSerializer
+
+
+@api_view()
+def product_list(request: Request):
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+
+@api_view()
+def product_details(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
